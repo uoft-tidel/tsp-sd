221f8f69fba4e9856bfad4252c6445c7536b8266
danpekar <pekar.daniel@outlook.com>
2024-12-10 19:15:04 -0500

    DIDP-Add works for path,
    Need to add first_visitied_node to state variable so it tracks last transition back to the first node to complete the cycle

--- src/DIDP.py
+++ src/DIDP.py
@@ -1,83 +1,163 @@
 import didppy as dp
+import math
+import json
+import os 
+import csv
+import validate as vlad
+import visualize as viz
 
-# Number of locations
-n = 4
-# Travel time
-w = [
-    [0, 3, 4, 5],
-    [3, 0, 5, 4],
-    [4, 5, 0, 3],
-    [5, 4, 3, 0],
-]
+def main (fpath, opt, export_results = False):
 
-model = dp.Model(maximize=False, float_cost=False)
+    with open(fpath, 'r') as file:
+      instance = json.load(file)
 
-customer = model.add_object_type(number=n)
+    def convertToLatLong(x):
+        deg = round(x)
+        minute = x-deg
+        lat = (math.pi*(deg+5*minute)/3)/180
+        return lat
 
-# U
-unvisited = model.add_set_var(object_type=customer, target=list(range(1, n)))
-# i
-location = model.add_element_var(object_type=customer, target=0)
-# t (resource variable)
-time = model.add_int_resource_var(target=0, less_is_better=True)
+    def getDistance(instance, p1,p2):
+        if p1 == '0' or p2 == '0':
+            return 0    
+        else:
+            p1 = instance["NODE_COORDS"][p1]
+            p2 = instance["NODE_COORDS"][p2]
 
-delete_table = model.add_set_table([[[], [], [], [2]], 
-     [[], [], [0], []],
-     [[], [0], [], [1,3]],
-     [[2], [], [1,3], []]], object_type=customer)
-travel_time = model.add_int_table(w)
+            # GEOMETRIC DISTANCE - NOT IMPLEMENTED IN PAPER
 
-for j in range(1, n):
-    visit = dp.Transition(
-        name="visit {}".format(j),
-        cost=travel_time[location, j] + dp.IntExpr.state_cost(),
-        preconditions=[unvisited.contains(j), delete_table[location,j].issuperset(unvisited)],
+            # RRR = 6378.388
+
+            # q1 = math.cos(convertToLatLong(p1[1]) - convertToLatLong(p2[1]))
+            # q2 = math.cos(convertToLatLong(p1[0]) - convertToLatLong(p2[0]))
+            # q3 = math.cos(convertToLatLong(p1[0]) + convertToLatLong(p2[0]))
+            # dij = round((RRR*math.acos(0.5*((1.0+q1)*q2 - (1.0-q1)*q3))+1.0))
+
+            # CARTESIAN DISTANCE
+
+            dij = math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
+
+            return round(dij*1000)
+
+    del_node = instance["DELETE"]
+
+    # Number of locations
+    n = len(instance["NODE_COORDS"].keys())+1 #+1 for dummy start
+    # Travel time
+    # c = [[0, 0, 0, 0, 0],
+    #     [0, 0, 10, 4, 2],
+    #     [0, 2, 0, 11, 4],
+    #     [0, 4, 2, 0, 12],
+    #     [0, 13, 4, 8, 0]
+    # ]
+
+    #del_node = {1:[(1,3), (1,4)],2:[(1,2),(2,4)],3:[(1,3), (2,3)],4:[(2,4),(1,2),(2,3),(1,4)]}
+    del_arc = [[set() for j in range(n)] for i in range(n)]
+
+    for node in del_node:
+        for [i, j] in del_node[node]:
+            i = int(i)
+            j = int(j)
+            del_arc[i][j].add(int(node))
+            del_arc[j][i].add(int(node))
+
+    del_arc = [[list(j) for j in i] for i in del_arc]
+
+
+    # Travel time
+    #w_i,j
+    c = [[getDistance(instance,str(i),str(j)) for j in range(n)] for i in range(n)]
+
+    model = dp.Model(maximize=False, float_cost=False)
+
+    customer = model.add_object_type(number=n)
+
+    # U
+    unvisited = model.add_set_var(object_type=customer, target=list(range(1, n)))
+    # i
+    location = model.add_element_var(object_type=customer, target=0)
+    # first visited location
+    # first = model.add_element_var(object_type=customer, target=0)
+    # t (resource variable)
+    time = model.add_int_resource_var(target=0, less_is_better=True)
+    # del table
+    d = model.add_set_table(del_arc, object_type=customer)
+
+    travel_time = model.add_int_table(c)
+
+    for j in range(1, n):
+        visit = dp.Transition(
+            name="visit {}".format(j),
+            cost=travel_time[location, j] + dp.IntExpr.state_cost(),
+            preconditions=[unvisited.contains(j), d[location,j].issubset(unvisited)], #first != 0, 
+            effects=[
+                (unvisited, unvisited.remove(j)),
+                (location, j),
+                (time, time + travel_time[location, j]),
+            ],
+        )
+        model.add_transition(visit)
+
+    # for j in range(1, n):
+    #     first_visit = dp.Transition(
+    #         name="first_visit {}".format(j),
+    #         cost=0, #travel_time[location, j] + dp.IntExpr.state_cost(),
+    #         #preconditions=[time == 0, set(range(1,n)) == unvisited],
+    #         effects=[
+    #             (unvisited, unvisited.remove(j)),
+    #             (location, j),
+    #             (first, j)
+    #             #(time, time + travel_time[location, j]),
+    #         ],
+    #     )
+    #     model.add_transition(first_visit)
+
+    return_to_depot = dp.Transition(
+        name="return",
+        cost=travel_time[location, 0] + dp.IntExpr.state_cost(),
         effects=[
-            (unvisited, unvisited.remove(j)),
-            (location, j),
-            (time, time + travel_time[location, j]),
+            (location, 0),
         ],
+        preconditions=[unvisited.is_empty(), location != 0], #d[location,first].issubset(unvisited)
     )
-    model.add_transition(visit)
+    model.add_transition(return_to_depot)
 
-return_to_depot = dp.Transition(
-    name="return",
-    cost=travel_time[location, 0] + dp.IntExpr.state_cost(),
-    effects=[
-        (location, 0),
-        (time, time + travel_time[location, 0]),
-    ],
-    preconditions=[unvisited.is_empty(), location != 0],
-)
-model.add_transition(return_to_depot)
+    model.add_base_case([unvisited.is_empty(), location == 0])
 
-model.add_base_case([unvisited.is_empty(), location == 0])
+    # State constraint for TSPSD
+    # for j in range(1, n):
+    #     model.add_state_constr(
+    #         ~unvisited.contains(j) | (time + travel_time[location, j] <= due_time[j])
+    #     )
 
-for j in range(1, n):
-    model.add_state_constr(
-        ~unvisited.contains(j)
+    min_to = model.add_int_table(
+        [min(c[k][j] for k in range(n) if k != j) for j in range(n)]
     )
 
-min_to = model.add_int_table(
-    [min(w[k][j] for k in range(n) if k != j) for j in range(n)]
-)
+    model.add_dual_bound(min_to[unvisited] + (location != 0).if_then_else(min_to[0], 0))
 
-model.add_dual_bound(min_to[unvisited] + (location != 0).if_then_else(min_to[0], 0))
+    min_from = model.add_int_table(
+        [min(c[j][k] for k in range(n) if k != j) for j in range(n)]
+    )
 
-min_from = model.add_int_table(
-    [min(w[j][k] for k in range(n) if k != j) for j in range(n)]
-)
+    model.add_dual_bound(
+        min_from[unvisited] + (location != 0).if_then_else(min_from[location], 0)
+    )
 
-model.add_dual_bound(
-    min_from[unvisited] + (location != 0).if_then_else(min_from[location], 0)
-)
+    solver = dp.CABS(model)
+    solution = solver.search()
 
-solver = dp.CABS(model)
-solution = solver.search()
+    print("Transitions to apply:")
 
-print("Transitions to apply:")
+    for t in solution.transitions:
+        print(t.name)
 
-for t in solution.transitions:
-    print(t.name)
+    print("Cost: {}".format(solution.cost))
 
-print("Cost: {}".format(solution.cost))
+folderpath = os.getcwd()
+instance = "burma14-3.1"
+fname = os.path.join(folderpath,"instances",instance+".json")
+
+opts = []
+
+main(fname, "none", export_results = True)
